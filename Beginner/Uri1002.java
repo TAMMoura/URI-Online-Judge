04e3f8ed5537f2e7fa6e6146d7874eb01194ac98
Thiago Moura <thiago_moura@hotmail.com.br>
2019-04-03 21:25:38 -0300

    Adicionando resolução da questão

--- Beginner/Uri1002.java
+++ Beginner/Uri1002.java
@@ -1,16 +1,17 @@
+package Beginner;
+
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Uri1002 {
 
 	public static void main(String[] args) {
-		
+		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+		sc.useLocale(Locale.US);
 		double raio = sc.nextDouble();
-		double pi = 3.14159;
-		double area = pi * (raio * raio);
-		
-		System.out.println("A= %1.4f" + area );
+		double area = 3.14159 * (Math.pow(raio, 2));
+		sc.close();
+		System.out.printf("A=%.4f\n", area);
 	}
-
 }
