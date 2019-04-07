6e463e508f4fd9a1a4982caf07f14a53e460c913
Thiago Moura <thiago_moura@hotmail.com.br>
2019-04-07 15:59:42 -0300

    Add resolution

--- beginner/Uri1001.java
+++ beginner/Uri1001.java
@@ -1,10 +1,16 @@
 package beginner;
 
+import java.util.Scanner;
+
 public class Uri1001 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+        Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int x = a + b;
+		sc.close();
+		System.out.println("X = " + x);
 	}
-
 }
