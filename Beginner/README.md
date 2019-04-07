4e19a07e618b44d5d6c09261a9631b296fdc9f19
Thiago Moura <thiago_moura@hotmail.com.br>
2019-04-07 00:57:12 -0300

    Add folder Beginner for better organization

---
+++ Beginner/README.md
@@ -0,0 +1,2 @@
+# URI-Online-Judge
+My solutions to problems from www.urionlinejudge.com.br
---
+++ Beginner/Uri1037.java
@@ -0,0 +1,31 @@
+package Beginner;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Uri1037 {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		double inter = sc.nextDouble();
+		
+		if (inter>=0 && inter <= 25) {
+			System.out.println("Intervalo [0,25]");
+		}
+		else if (inter > 25 && inter<= 50) {
+			System.out.println("Intervalo (25,50]");
+		}
+		else if (inter > 50 && inter <= 75) {
+			System.out.println("Intervalo (50,75]");
+		}
+		else if (inter > 75 && inter <= 100) {
+			System.out.println("Intervalo (75,100]");
+		}
+		else {
+			System.out.println("Fora de intervalo");
+		}
+	}
+
+}
