37a6f33c39d8b02a45e17e2ae1dfdf270e9d9115
Thiago Moura <thiago_moura@hotmail.com.br>
2019-04-12 20:57:04 -0300

    Add resolution

---
+++ beginner/Uri1053.java
@@ -0,0 +1,11 @@
+package beginner;
+
+public class Uri1053 {
+
+	public static void main(String[] args) {
+		int number = 0; 
+		for (number = 2; number <= 100; number += 2 ) {
+			System.out.println(number);
+		}
+	}
+}
