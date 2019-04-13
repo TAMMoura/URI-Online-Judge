4d985231d0760037721676f5f2f456a8783f558c
Thiago Moura <thiago_moura@hotmail.com.br>
2019-04-12 21:12:53 -0300

    Add resolution

---
+++ beginner/Uri1060.java
@@ -0,0 +1,22 @@
+package beginner;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Uri1060 {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		int cont = 0;
+		Scanner sc = new Scanner(System.in);
+		
+		for (int i = 0; i < 6; i ++) {
+			double a = sc.nextDouble();
+			if (a > 0) {
+				cont++;
+			}	
+		}
+	
+		System.out.printf("%d valores positivos\n", cont);
+	}
+}
