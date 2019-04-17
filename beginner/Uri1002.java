a4fd828a1587e6556374f5ea899914938c06cc37
Thiago Moura <thiago_moura@hotmail.com.br>
2019-04-16 22:00:16 -0300

    Adicionando comentários em Português

--- beginner/Uri1002.java
+++ beginner/Uri1002.java
@@ -10,10 +10,20 @@ public class Uri1002 {
     public static void main(String[] args) throws IOException {
     	
 		Locale.setDefault(Locale.US);
+		
+		//Invocar a classe Scanner
 		Scanner sc = new Scanner(System.in);
+		
+		//Entrar com o valor do raio
 		double raio = sc.nextDouble();
+		
+		//Calcular a area de acordo com o valor do raio digitado
 		double area = 3.14159 * (Math.pow(raio, 2));
+		
+		//Fechar o Scanner
 		sc.close();
+		
+		//Imprimindo a solução de acordo com a saída proposta no exercício
 		System.out.printf("A=%.4f\n", area);
 	}
 }
