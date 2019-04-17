33919d2c456f52375bbb0a0e1b1ad06c917dd51e
Thiago Moura <thiago_moura@hotmail.com.br>
2019-04-16 21:56:21 -0300

    Add resolution

--- beginner/Uri1002.java
+++ beginner/Uri1002.java
@@ -8,9 +8,9 @@ public class Uri1002 {
 
 	 
     public static void main(String[] args) throws IOException {
+    	
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		sc.useLocale(Locale.US);
 		double raio = sc.nextDouble();
 		double area = 3.14159 * (Math.pow(raio, 2));
 		sc.close();
