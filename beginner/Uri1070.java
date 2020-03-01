243277864c0cd67fa10d4e501cd7ac74312d36bf
TAMMoura <thiago_moura@hotmail.com.br>
2020-03-01 20:20:04 -0300

    Add resolution

---
+++ beginner/Uri1070.java
@@ -0,0 +1,22 @@
+package beginner;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Uri1070 {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		if (num%2 ==0) {
+			num +=1;
+		}
+		for (int i= 0; i < 5; i++) {
+			System.out.println(num);
+			num = num + 2;
+		}
+		System.out.println(num);
+
+	}
+}
