2448bd1169d0549103ef94c07752377d85ee8d1b
TAMMoura <thiago_moura@hotmail.com.br>
2020-03-01 19:42:36 -0300

    Add resolution

---
+++ beginner/Uri1065.java
@@ -0,0 +1,20 @@
+package beginner;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Uri1065 {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		int count = 0;
+		for (int i = 0; i < 5; i++) {
+			float num = sc.nextFloat();
+			if (num%2 == 0) {
+				count +=1;
+			}
+		}
+		System.out.println(count + " valores pares");
+	}
+}
