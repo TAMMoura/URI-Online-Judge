79fb74a93ca96bec2f057ff46d467e458adf7edf
TAMMoura <thiago_moura@hotmail.com.br>
2020-03-01 23:43:54 -0300

    Add resolution

--- beginner/Uri1078.java
+++ beginner/Uri1078.java
@@ -3,16 +3,14 @@ package beginner;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Uri1075 {
+public class Uri1078 {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		for (int i = 0; i < 10000; i++) {
-			if (i%N == 2) {
-				System.out.println(i);
-			}
+		for (int i = 1; i < 11; i++) {
+			System.out.println(i + " x " + N + " = " + i * N);
 		}
 	}
 
