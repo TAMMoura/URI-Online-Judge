98e2e7814a36137897dfebb7c954f14fe2776b20
TAMMoura <thiago_moura@hotmail.com.br>
2020-03-01 23:33:38 -0300

    Add resolution

---
+++ beginner/Uri1074.java
@@ -0,0 +1,37 @@
+package beginner;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Uri1074 {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		int number;
+		int N = sc.nextInt();
+		for (int i = 0; i < N; i++) {
+			number = sc.nextInt();
+			
+			if (number == 0) {
+				System.out.println("NULL");
+			}
+			else {
+				if (number % 2 == 0) {
+					if (number > 0) {
+						System.out.println("EVEN POSITIVE");
+					}
+					else {
+						System.out.println("EVEN NEGATIVE");
+					}
+				}
+				else if (number > 0) {
+						System.out.println("ODD POSITIVE");
+					}	
+					else {
+						System.out.println("ODD NEGATIVE");
+					}
+			}	
+		}
+	}
+}
