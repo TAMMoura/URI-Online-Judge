0402cb5d1ebc0c82cbe0bd3e2ddb5a14b2ef4932
TAMMoura <thiago_moura@hotmail.com.br>
2020-03-02 23:28:51 -0300

    Add resolution

---
+++ beginner/Uri1095.java
@@ -0,0 +1,11 @@
+package beginner;
+
+public class Uri1095 {
+
+	public static void main(String[] args) {
+		for(int i = 1, j = 60; j >= 0; i +=3, j-=5) {
+			System.out.printf("I=" + i + " J=" + j + "\n");
+
+		}
+	}
+}
