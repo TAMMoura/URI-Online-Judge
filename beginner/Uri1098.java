8ad704039bc9d5d11830d116d9c4e2542bf50e60
TAMMoura <thiago_moura@hotmail.com.br>
2020-03-02 23:45:03 -0300

    Add resolution

---
+++ beginner/Uri1098.java
@@ -0,0 +1,26 @@
+package beginner;
+
+import java.text.DecimalFormat;
+
+public class Uri1098 {
+
+	public static void main(String[] args) {
+		DecimalFormat df = new DecimalFormat("0.0");
+		double i = 0;
+		double j = 1;
+		while (i <= 2) {
+			for (int k = 0; k < 3; k++) {
+				if (df.format(i).endsWith("0")) {
+					System.out.println("I=" + df.format(i).substring(0, 1) + " J=" + df.format(j).substring(0, 1));
+				} else {
+					System.out.println("I=" + df.format(i) + " J=" + df.format(j));
+				}
+				j++;
+			}
+			j-=3;
+			j+=0.2;
+			i+=0.2;
+		}
+
+	}
+}
