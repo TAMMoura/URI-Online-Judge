8fd64b9e1f7f224d999b02822f3a9c6fd60e42cb
TAMMoura <thiago_moura@hotmail.com.br>
2020-03-04 23:33:23 -0300

    Add resolution

--- beginner/Uri1002.java
+++ beginner/Uri1002.java
@@ -11,19 +11,14 @@ public class Uri1002 {
     	
 		Locale.setDefault(Locale.US);
 		
-		//Invocar a classe Scanner
 		Scanner sc = new Scanner(System.in);
 		
-		//Entrar com o valor do raio
 		double raio = sc.nextDouble();
 		
-		//Calcular a area de acordo com o valor do raio digitado
 		double area = 3.14159 * (Math.pow(raio, 2));
 		
-		//Fechar o Scanner
 		sc.close();
 		
-		//Imprimindo a solução de acordo com a saída proposta no exercício
 		System.out.printf("A=%.4f\n", area);
 	}
 }
