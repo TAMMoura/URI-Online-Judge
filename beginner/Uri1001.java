177b872065c57ef35783dcaef2e2276b5b0d1671
TAMMoura <thiago_moura@hotmail.com.br>
2020-03-04 23:34:39 -0300

    Add resolution

--- beginner/Uri1001.java
+++ beginner/Uri1001.java
@@ -6,22 +6,11 @@ public class Uri1001 {
 
 	public static void main(String[] args) {
 		
-		//Lendo os dois números inteiros do teclado com a classe Scanner
         Scanner sc = new Scanner(System.in);
-        
-        //Entrando com o primeiro número inteiro
-		int a = sc.nextInt();
-		
-		//Entrando com o segundo número inteiro
+      	int a = sc.nextInt();
 		int b = sc.nextInt();
-		
-		//Declarando uma variável x e atribuindo a ela o somatório da variável a com a variável b
 		int x = a + b;
-		
-		//Fechando a classe Scanner
 		sc.close();
-		
-		//Imprimindo a solução de acordo com a saída proposta no exercício
 		System.out.println("X = " + x);
 	}
 }
