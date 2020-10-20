76d91c026f85f46d78147ccbd1fc661356ffdd4a
Thiago Moura <thiago_moura@hotmail.com.br>
2020-10-20 13:36:02 -0300

    Add resolution

---
+++ src/beginner/Uri1059.java
@@ -0,0 +1,14 @@
+package beginner;
+
+import java.io.IOException;
+
+public class Uri1059 {
+
+	public static void main(String[] args) throws IOException {
+	        
+		int number = 0; 
+		for (number = 2; number <= 100; number += 2 ) {
+			System.out.println(number);
+		}
+	}
+}
