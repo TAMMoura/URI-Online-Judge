6b933485eda82ac8899385f1cae65ce87f838024
TAMMoura <thiago_moura@hotmail.com.br>
2022-06-07 07:14:00 -0300

    Add 1008.py

---
+++ src/beginner/Uri1008.py
@@ -0,0 +1,6 @@
+numero = int(input())
+horasTrabalhadas = int(input())
+valorHora = float(input())
+salario = horasTrabalhadas * valorHora
+print("NUMBER =" , numero)
+print("SALARY = U$ %1.2f" %salario)
