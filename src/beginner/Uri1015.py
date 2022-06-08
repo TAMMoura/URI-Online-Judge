6d56e76c6db19cf4df66acb7208b44bcd1da6aa8
TAMMoura <thiago_moura@hotmail.com.br>
2022-06-08 08:09:22 -0300

    Add 1015 py

---
+++ src/beginner/Uri1015.py
@@ -0,0 +1,9 @@
+linha1 = input().split(" ")
+linha2 = input().split(" ")
+
+x1 , y1 = linha1
+x2 , y2 = linha2
+
+distancia = ((float(x2) - float(x1))* (float(x2) - float(x1)) + (float(y2) - float(y1)) *(float(y2) - float(y1)))** (1/2)
+
+print("%0.4f" %distancia)
